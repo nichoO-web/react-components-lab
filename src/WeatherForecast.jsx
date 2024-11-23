7765ad4983151c68821e9b24affb04fe2fa1dec4
nichoOtero <nichonaks@gmail.com>
2024-11-23 15:41:39 -0600

    react-components-lab

---
+++ src/WeatherForecast.jsx
@@ -0,0 +1,10 @@
+export default function WeatherForecast(props) {
+    return (
+        <div className="weather">
+            <h2>{props.day}</h2>
+            <img src={props.img} alt={props.imgAlt} />
+            <p><span>conditions: </span>{props.conditions}</p>
+            <p><span>time: </span>{props.time}</p>
+        </div>
+    );
+}
